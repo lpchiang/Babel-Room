1c119d4314f8518c991df8ce1ee00b67ca19e3ef
Lucas <lucaspolo.chiang@gmail.com>
2025-04-16 00:08:34 -0300

    Modificação do componente ThirdPartyLogin.tsx: - Renomeação da constante ClickableIcons para ThirdPartyIcons - Uso de desestruturação nos parâmetros da arrow function

--- src/components/ThirdPartyLogin.tsx
+++ src/components/ThirdPartyLogin.tsx
@@ -4,15 +4,15 @@ import GOOGLE from '../constants/google/google';
 import FACEBOOK from '../constants/facebook/facebook';
 import LINKEDIN from '../constants/linkedin/linkedin';
 
-const ClickableIcons: ClickableIcon[] = [APPLE, GOOGLE, FACEBOOK, LINKEDIN];
+const ThirdPartyIcons: ClickableIcon[] = [APPLE, GOOGLE, FACEBOOK, LINKEDIN];
 
 const ThirdPartyLogin: React.FC = () => {
     return (
         <ul>
-            {ClickableIcons.map((item) => (
+            {ThirdPartyIcons.map(({img, url}) => (
                 <li className='third-party-icons'>
-                    <a href={item.url.href}>
-                        <img src={item.img} alt=""/>
+                    <a href={url.href}>
+                        <img src={img} alt=""/>
                     </a>
                 </li>
             ))
