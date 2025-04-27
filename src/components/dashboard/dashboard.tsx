34cb082d5670005f826a7b25a5cbac5605f75fe6
Lucas <lucaspolo.chiang@gmail.com>
2025-04-27 15:38:40 -0300

    - Modificações no componente dashboard

---
+++ src/components/dashboard/dashboard.tsx
@@ -0,0 +1,22 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card"
+
+
+const Dashboard = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>
+          LESSONS
+        </CardTitle>
+        <CardDescription>
+          LESSON 1
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+
+      </CardContent>
+    </Card>
+  )
+}
+
+export default Dashboard
