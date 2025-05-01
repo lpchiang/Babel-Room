28261f17f68edac18f7aae7ad1ceb13e5dfa78df
Guilherme Hugo de Araujo Silva <114270447+guihugo@users.noreply.github.com>
2025-05-01 23:10:30 +0000

    add controller

--- src/controller/signUp_controller.tsx
+++ src/controller/signUp_controller.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const API_URL = 'http://localhost:5092/api/users/signUp';
+const API_URL = 'http://3:142:243:38:5000/api/users/signUp';
 
 export function useSignUpController() {
   const [loading, setLoading] = useState(false);
