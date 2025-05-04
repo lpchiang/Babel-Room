a91f798df2f3c184544eb9e86d997d62ffc700a7
guihugo <guilherme.hugo@aluno.ufabc.edu.br>
2025-05-03 23:31:19 -0300

    fix url

--- src/controller/login_controller.tsx
+++ src/controller/login_controller.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const API_URL = 'https://babelroom.click/api/users/login';
+const API_URL = 'http://3.142.243.38:5000/api/users/login';
 
 
 export function logInController() {
--- src/data/lessonQuestionIds.ts
+++ src/data/lessonQuestionIds.ts
@@ -1,3 +1,3 @@
 export const lessonQuestionIds: Record<string, string[]> = {
-    "lesson1": ["0195f3f3-d45a-7b4a-a9e9-c4b1286db965", "019679e3-dfa0-7ebe-9f9a-64b9302f0609", "019679e4-3a09-7e9a-acf6-d85e329a68e6", "019679e4-6ff3-7be1-adc2-11c104551652"],
+    "lesson1": ["019698ef-9987-79bc-8644-aa842d5205ff", "019698ef-d4ba-718d-80ea-21c35e3d84ac", "019698ef-f6f4-7145-a1a7-453670582b6d", "019698f0-1957-7246-a8a6-26b709d5eaf4", "019698f0-3c9a-7349-8a00-22442a63da54"],
   };
